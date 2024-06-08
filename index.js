e958fd4d8b1effadad83dd4db9f80b352528e688
Dominictamondong <tamondongminic3@gmail.com>
2024-06-08 16:19:52 +0800

    Update index.js

--- index.js
+++ index.js
@@ -30,8 +30,9 @@ const comment = document.getElementById("comment")
 const submit = document.getElementById("submit")
 const commentbox = document.getElementById("commentbox")
 
-function submitComment(){
-commentbox.textContent += comment.value.toString() + "\n"
-}
+function submitComment() {
+   commentbox.textContent += comment.value.toString() + "\n"
+    comment.value=""
+
 }
 submit.addEventListener("click",submitComment)
